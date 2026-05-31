f7625155ce8a97df7d90fcdc09d28dfb9c21c63f
YUGRAJ MEWARA <yugrajmewara08@gmail.com>
2026-05-31 14:51:55 +0530

    Solve typo erro

--- src/main/java/com/yugraj/financebackend/controller/RecordController.java
+++ src/main/java/com/yugraj/financebackend/controller/RecordController.java
@@ -60,7 +60,7 @@ public class RecordController {
 
     //Get ALL (Filter + Pagination)
     @GetMapping
-    @PreAuthorize("hasRole('isAuthenticated()')")
+    @PreAuthorize("isAuthenticated()")
     public List<RecordResponseDTO> getAllRecords(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
