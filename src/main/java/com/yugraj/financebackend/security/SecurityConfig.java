5d3fee7dba43219318be7329eeae0c604d3e8e0d
YUGRAJ MEWARA <yugrajmewara08@gmail.com>
2026-06-24 16:07:21 +0530

    Allow Vercel Url

--- src/main/java/com/yugraj/financebackend/security/SecurityConfig.java
+++ src/main/java/com/yugraj/financebackend/security/SecurityConfig.java
@@ -47,7 +47,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:5173",
+                "https://*.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
